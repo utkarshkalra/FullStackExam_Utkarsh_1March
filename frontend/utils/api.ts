96d9694071566684d2427cda6a066562fc2e1d01
utkarshkalra <utkarshkalra.2001@gmail.com>
2025-02-28 14:59:45 +0530

    changing api url

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "https://fullstackexam-utkarsh-1march.onrender.com/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5123/api";
 
 const api = axios.create({
   baseURL: API_URL,
